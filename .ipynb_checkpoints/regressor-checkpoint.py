89a74e0519ab931615fceca2faa5137ae5760958
marcelo velludo <velludom@gmail.com>
2021-09-14 02:23:13 -0300

    Alterações nomes das tabelas e criação de 3 experimentos

--- .ipynb_checkpoints/regressor-checkpoint.py
+++ .ipynb_checkpoints/regressor-checkpoint.py
@@ -15,19 +15,14 @@ bz_2 = (pi/16)*(10**3)*mhz
 j_12 = pi/140
 
 t0 = perf_counter()
-#dpq = DPQNova.DinamicaPontosQuanticos(j_1_inicial= j_1*0.8, j_1_final= j_1*1.2, passoJ_1 = 0.1,
-#                                      j_2_inicial= j_2*0.8, j_2_final= j_2*1.2, passoJ_2 = 0.1,
-#                                      bz_1_inicial= bz_1*0.8, bz_1_final= bz_1*1.2, passoBz_1 = 0.01,
-#                                      bz_2_inicial= bz_2*0.8, bz_2_final= bz_2*1.2, passoBz_2 = 0.01,
-#                                      j_12_inicial= j_12*0.8, j_12_final= j_12*1.2, passoJ_12 = 0.0001,
-#                                      tInicial=5, tFinal=25, passoT=5)
 
-dpq = DPQNova.DinamicaPontosQuanticos(j_1_inicial=1, j_1_final=1, passoJ_1 = 0.5,
-                 j_2_inicial=1, j_2_final=1, passoJ_2 = 0.2,
-                 bz_1_inicial=0.1, bz_1_final=10, passoBz_1 = 1.0,
-                 bz_2_inicial=0.1, bz_2_final=10, passoBz_2 = 1.0,
-                 j_12_inicial=0.1, j_12_final=10, passoJ_12 = 1.0,
-                 tInicial=5, tFinal=25, passoT=5)
+dpq = DPQNova.DinamicaPontosQuanticos(j_1_inicial= j_1*0.8, j_1_final= j_1*1.2, passoJ_1 = 0.1,
+                                      j_2_inicial= j_2*0.8, j_2_final= j_2*1.2, passoJ_2 = 0.1,
+                                      bz_1_inicial= bz_1*0.8, bz_1_final= bz_1*1.2, passoBz_1 = 0.01,
+                                      bz_2_inicial= bz_2*0.8, bz_2_final= bz_2*1.2, passoBz_2 = 0.01,
+                                      j_12_inicial= j_12*0.8, j_12_final= j_12*1.2, passoJ_12 = 0.0001,
+                                      tInicial=5, tFinal=25, passoT=5)
+
 df = dpq.criaDataFrame()
 t1 = perf_counter()
 
@@ -41,12 +36,14 @@ mse = mean_squared_error(y_train, y_train_pred)
 r2 = r2_score(y_train,y_train_pred)
 
 with open("Speed.txt", "a+") as text_file:
+    text_file.write("===="*5)
+    text_file.write("\n%s"%(dpq.name_comp))
     text_file.write("Tempo tabela: %f \nTempo regressor: %f \n" % (t1-t0,t2-t1))
 
 
 with open("ResultadosTreino.txt", "a+") as text_file:
     text_file.write("===="*5)
-    text_file.write("\n%s"%(dpq.name))
+    text_file.write("\n%s"%(dpq.name_comp))
     text_file.write("\nMédia do erro absoluto: %f \nMédia quadrada do erro: %f \nR2: %f\n" % (mae, mse, r2))
     
 graphics.plotGraph(y_train,y_train_pred, "Extra Trees Regressor Train",dpq.name, mae, mse, r2)
@@ -59,10 +56,12 @@ r2 = r2_score(y_test,y_test_pred)
 
 with open("ResultadosTest.txt", "a+") as text_file:
     text_file.write("===="*5)
-    text_file.write("\n%s"%(dpq.name))
+    text_file.write("\n%s"%(dpq.name_comp))
     text_file.write("\nMédia do erro absoluto: %f \nMédia quadrada do erro: %f \nR2: %f \n" % (mae, mse, r2))
 
-graphics.plotGraph(y_test,y_test_pred, "Extra Trees Regressor Test",dpq.name ,mae, mse, r2)
+graphics.plotGraph(y_test,y_test_pred, "Extra Trees Regressor Test",dpq.name_comp ,mae, mse, r2)
 
 dpq.saveDataFrame()
+
 dpq.save_Y(y_test, y_test_pred, len(y_test))
+
--- regressor-Copy2.py
+++ regressor-Copy2.py
@@ -16,12 +16,12 @@ j_12 = pi/140
 
 t0 = perf_counter()
 
-dpq = DPQNova.DinamicaPontosQuanticos(j_1_inicial= j_1*0.8, j_1_final= j_1*1.2, passoJ_1 = 0.1,
-                                      j_2_inicial= j_2*0.8, j_2_final= j_2*1.2, passoJ_2 = 0.1,
-                                      bz_1_inicial= bz_1*0.8, bz_1_final= bz_1*1.2, passoBz_1 = 0.01,
-                                      bz_2_inicial= bz_2*0.8, bz_2_final= bz_2*1.2, passoBz_2 = 0.01,
-                                      j_12_inicial= j_12*0.8, j_12_final= j_12*1.2, passoJ_12 = 0.0001,
-                                      tInicial=5, tFinal=25, passoT=5)
+dqp = DPQNova.DinamicaPontosQuanticos(j_1_inicial=1, j_1_final=1, passoJ_1 = 0.5,
+                 j_2_inicial=1, j_2_final=1, passoJ_2 = 0.5,
+                 bz_1_inicial=0.1, bz_1_final=10, passoBz_1 = 1.0,
+                 bz_2_inicial=0.1, bz_2_final=10, passoBz_2 = 1.0,
+                 j_12_inicial=0.1, j_12_final=10, passoJ_12 = 1.0,
+                 tInicial=5, tFinal=25, passoT=5)
 
 df = dpq.criaDataFrame()
 t1 = perf_counter()
